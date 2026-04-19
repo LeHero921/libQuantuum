78b1a5eb5d898b14c1b7b2b4dcd0fd0dc0da088e
LeHero921 <134062525+LeHero921@users.noreply.github.com>
2026-04-19 14:16:50 +0200

    fix i guess

--- src/core.cpp
+++ src/core.cpp
@@ -11,15 +11,15 @@ bool Core::IsValidRect(const Rectangle& rect) {
     return rect.width != 0 && rect.height != 0;
 };
 
-bool Core::IsValidSprite(const Sprite& sprite) {
-    return IsValidTexture(sprite.texture) &&
-           IsValidRect(sprite.dest_rect) &&
-           IsValidRect(sprite.source_rect);
-};
+// bool Core::IsValidSprite(const Sprite& sprite) {
+//     return IsValidTexture(sprite.texture) &&
+//            IsValidRect(sprite.dest_rect) &&
+//            IsValidRect(sprite.source_rect);
+// };
 
-bool Core::IsValidRenderStructure(const RenderStructure& rs) {
-    return IsValidSprite(rs.sprite);
-};
+// bool Core::IsValidRenderStructure(const RenderStructure& rs) {
+//     return IsValidSprite(rs.sprite);
+// };
 
 bool Core::valid_glCtx()
 {
